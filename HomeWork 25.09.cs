2664e4e8b88bac914b47082ff3585d39c1adfb15
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-09-25 20:34:55 +0500

    Add files via upload

---
+++ HomeWork 25.09.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdfasdfasdf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координату x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y: ");
+            double y = double.Parse(Console.ReadLine());
+            bool result = ((y <= 4) && ((Math.Pow(x, 2) + Math.Pow(y, 2) >= 16) && (y >= -x)) || ((y <= 4) && (Math.Pow(x, 2) + Math.Pow(y, 2) >= 16) && (y <= x)));
+            if (result == true)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
